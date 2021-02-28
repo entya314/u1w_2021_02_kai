b104953d5cf2849e3f0ddd700bd7983f710bbea3
entya314 <entya@entya>
2021-02-28 10:18:59 +0900

    音楽とか

---
+++ Assets/Script/Title/Title.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title : MonoBehaviour
+{
+    public BaseGear ha;
+    public BaseGear guruma;
+    public BaseGear pa;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ha.RotateGear(Time.deltaTime, 1);
+        guruma.RotateGear(Time.deltaTime, -1);
+        pa.RotateGear(Time.deltaTime, 1);
+    }
+}
